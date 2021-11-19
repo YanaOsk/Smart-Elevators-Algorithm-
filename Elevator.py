4e42173b1078f20ba7406814f1b58b08e96aa118
YanaOsk <yanaoskin35@gmail.com>
2021-11-19 19:02:19 +0200

    final

--- Elevator.py
+++ Elevator.py
@@ -1,5 +1,5 @@
 class Elevator:
-    def __init__(self,id,speed,minFloor,maxFloor,closeTime,openTime,startTime,stopTime):
+    def __init__(self,id,speed,minFloor,maxFloor,closeTime,openTime,startTime,stopTime, arr,lastpos):
 
         self.id = id
         self.speed = speed
@@ -9,12 +9,12 @@ class Elevator:
         self.openTime = openTime
         self.startTime = startTime
         self.stopTime = stopTime
+        self.arr = arr
+        self.lastpos = 0
 
 
     def __str__(self):
-        return f"_id:{self.id} _speed:{self.speed} _minFloor:{self.minFloor} _maxFloor:{self.maxFloor} _closeTime:{self.closeTime} _openTime:{self.openTime} _startTime:{self.startTime} _stopTime:{self.stopTime}"
+        return f"_id:{self.id} _speed:{self.speed} _minFloor:{self.minFloor} _maxFloor:{self.maxFloor} _closeTime:{self.closeTime} _openTime:{self.openTime} _startTime:{self.startTime} _stopTime:{self.stopTime} _arr:{self.arr}"
 
     def __repr__(self):
-        return f"_id:{self.id} _speed:{self.speed} _minFloor:{self.minFloor} _maxFloor:{self.maxFloor} _closeTime:{self.closeTime} _openTime:{self.openTime} _startTime:{self.startTime} _stopTime:{self.stopTime}"
-
-
+        return f"_id:{self.id} _speed:{self.speed} _minFloor:{self.minFloor} _maxFloor:{self.maxFloor} _closeTime:{self.closeTime} _openTime:{self.openTime} _startTime:{self.startTime} _stopTime:{self.stopTime} _arr:{self.arr}"
--- Ex1.py
+++ Ex1.py
@@ -10,19 +10,15 @@ def json_to_obj_elev(build_json):
     try:
         with open(build_json, "r+") as f:
             elv = []
-            # print(type(elv))
             elevator_dict = json.load(f)
             elev_for_obj = elevator_dict["_elevators"]
-            # print(e1)
+
+            lastpos = 0
             counter = 0
             for k in elev_for_obj:
                 e1 = Elevator(k["_id"], k["_speed"], k["_minFloor"], k["_maxFloor"], k["_closeTime"], k["_openTime"],
-                              k["_startTime"], k["_stopTime"])
-
-                # print(e2)
-                # print(type(e2))
+                              k["_startTime"], k["_stopTime"], [], lastpos)
                 elv.append(e1)
-
             return elv
     except IOError as e:
         print(e)
@@ -45,31 +41,29 @@ def csv_to_list_of_obj(calls_csv):
         csv_reader = csv.reader(f)
         header = next(csv_reader)
         for row in csv_reader:
-            c = CallForElevator(elv=str(row[0]), time=float(row[1]), src=int(row[2]), dest=int(row[3]), elv_pos=int(row[4]), alloc=int(row[5]))
-            calls.append(c) 
+            c = CallForElevator(elv=str(row[0]), time=float(row[1]), src=int(row[2]), dest=int(row[3]),
+                                elv_pos=int(row[4]), alloc=int(row[5]))
+            calls.append(c)
 
-    # print(temp )
     return calls
- 
-
-def insertDataCall_and_CalcTime(elv, calls):
-    id_elev = 0
-    time_arr = []
-    avg = 0
-    min_avg = 0
-    temp_dict = {}
-    for k, v in temp_dict.items():
-        temp_dict[k] = elv[k]
-        temp_dict[v] = calls[v]
-    print(temp_dict)
-    # for i,j in temp_dict.items():
-    #     floor = temp_dict[j]
-    #
 
 
-def insert_zero(calls):
+def allocate_id_to_call(calls, building):
+    all_calls_time = []
+    for i in building.elevators:
+        all_calls_time.append(0)
     for call in calls:
-        call.alloc = 0
+        min_time = sys.maxsize
+        id_elv = 0
+
+        for elvator in building.elevators:
+            calltime = calcTimeForOneCall(elvator, call)
+            if (calltime + all_calls_time[building.elevators.index(elvator)]) < min_time:
+                min_time = calcTimeForOneCall(elvator, call) + all_calls_time[building.elevators.index(elvator)]
+                id_elv = building.elevators.index(elvator)
+        all_calls_time[id_elv] += calcTimeForOneCall(building.elevators[id_elv], call)
+        building.elevators[id_elv].arr.append(call)
+        call.alloc = building.elevators[id_elv].id
 
 
 def make_out_file(calls, file_name):
@@ -81,21 +75,26 @@ def make_out_file(calls, file_name):
         csvwriter.writerows(new_list)
 
 
-if __name__ == '__main__':
-    # print(sys.argv)
-    # print(sys.argv[1])
-    b1 = json_to_obj(sys.argv[1])
-    print("Our Building: ")
-    print(b1)
-    print("---------------")
-    print("Our Elevators")
-    e1 = json_to_obj_elev(sys.argv[1])
-    print(e1)
-    print("------------")
-    print("Our Calls")
-    c1 = csv_to_list_of_obj(sys.argv[2])
-    print(c1)
-    print("----------------")
-    # insertDataCall_and_CalcTime(e1,c1)
-    insert_zero(c1)
-    make_out_file(c1, sys.argv[3])
+def calcTimeForOneCall(elv, call):
+    """Function that calculates how much time it takes to one call"""
+    stoptime = elv.closeTime + elv.startTime + elv.stopTime + elv.openTime
+    if isEmpty(elv.arr) == True:
+        time1 = abs((0 - call.src) ** elv.speed) + stoptime + abs((call.dest - call.src) * elv.speed) + stoptime
+    else:
+        time1 = abs((elv.arr[len(elv.arr) - 1].dest - call.src) * elv.speed) + stoptime + abs(
+            (call.dest - call.src) * elv.speed) + stoptime
+    return time1
+
+
+def isEmpty(arr):
+    """Function that checks if the elevators array is empty or not(if it have calls inside)"""
+    isEmpty = False
+    if len(arr) == 0:
+        isEmpty = True
+    return isEmpty
+
+
+building = json_to_obj(sys.argv[1])
+calls = csv_to_list_of_obj(sys.argv[2])
+allocate_id_to_call(calls, building)
+make_out_file(calls, sys.argv[3])
